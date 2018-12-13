5c1c5b9804d4623d0bb752cf91b570d5258f30f2
Elizabeth1937 <leetea2@gmail.com>
2018-12-13 08:38:08 +0400

    Посмотри праильно ли я сделала У меня на копьютере выдает ошибку в игре, не запускается

--- GAME.py
+++ GAME.py
@@ -1,6 +1,9 @@
 
 import tkinter
 import random
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from ui_file import Ui_MainWindow
 
 
 def prepare_and_start():
@@ -35,7 +38,48 @@ def prepare_and_start():
 
 
 def settings():
-    return (step)
+
+    class MyWidget(QMainWindow, Ui_MainWindow):
+        def __init__(self):
+            super().__init__()
+            self.setupUi(self)
+            self.pushButton.clicked.connect(self.run)
+
+        def run(self):
+            f = open('GAME_SETTINGS.txt', 'w')
+            # repson
+            f.write('R-2 D-2; BB-8; R-4; C-3 P O; Chopper' + '\n')
+            if self.radioButton_2.isChecked():
+                f.write('1' + '\n')
+            elif self.radioButton_3.isChecked():
+                f.write('2' + '\n')
+            elif self.radioButton_9.isChecked():
+                f.write('3' + '\n')
+            elif self.radioButton_4.isChecked():
+                f.write('4' + '\n')
+            elif self.radioButton.isChecked():
+                f.write('5' + '\n')
+            # rpotiv
+            f.write('Штурмовик; Штурмовик Смерти; Дроид торговой Федерации' + '\n')
+            if self.radioButton_6.isChecked():
+                f.write('1' + '\n')
+            elif self.radioButton_7.isChecked():
+                f.write('2' + '\n')
+            elif self.radioButton_8.isChecked():
+                f.write('3' + '\n')
+            # prepyat
+            f.write('Ямы; Мины; Баст-дроиды' + '\n')
+            if self.radioButton_10.isChecked():
+                f.write('1' + '\n')
+            elif self.radioButton_11.isChecked():
+                f.write('2' + '\n')
+            elif self.radioButton_5.isChecked():
+                f.write('3' + '\n')
+
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
 
 
 def always_right():
