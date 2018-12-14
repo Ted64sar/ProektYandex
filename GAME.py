4e518c06f5d9203c1e81b6af4b4e93f11d23495d
Smart Ted <fedor-commander@yandex.ru>
2018-12-14 16:08:35 +0400

    доделки

--- GAME.py
+++ GAME.py
@@ -7,6 +7,7 @@ from ui_file import Ui_MainWindow
 
 
 def prepare_and_start():
+
     label.config(text="Найди выход")
     f = []
     global player, exit, fires, enemies
@@ -17,7 +18,7 @@ def prepare_and_start():
     while exit_pos == player_pos:
         exit_pos = (random.randint(1, N_X - 1) * step, random.randint(1, N_Y - 1) * step)
     exit = canvas.create_image((exit_pos[0], exit_pos[1]), image=exit_pic, anchor='nw')
-    N_FIRES = 10
+    N_FIRES = 12
     fires = []
     for i in range(N_FIRES):
         fire_pos = (random.randint(1, N_X - 1) * step, random.randint(1, N_Y - 1) * step)
@@ -132,7 +133,7 @@ def check_move():
 def move_wrap(canvas, obj, move):
     canvas.move(obj, move[0], move[1])
     coords = canvas.coords(obj)
-    to_move_x = coords[0] % 900 - coords[0]
+    to_move_x = coords[0] % 720 - coords[0]
     to_move_y = coords[1] % 720 - coords[1]
     canvas.move(obj, to_move_x, to_move_y)
 
@@ -156,18 +157,19 @@ def key_pressed(event):
 
 step = 60
 N_X = 12
-N_Y = 15
+N_Y = 12
 master = tkinter.Tk()
 with open('GAME_SETTINGS.txt', 'r') as inp:
     f = inp.read().split('\n')[0::]
     files = f[::2]
     ind = f[1::2]
 
+
+label = tkinter.Label(master, text="Найди выход")
 player_pic = tkinter.PhotoImage(file="images/"+str(files[0].split()[int(ind[0])-1]))
 exit_pic = tkinter.PhotoImage(file="images/tardis.png")
 fire_pic = tkinter.PhotoImage(file="images/"+str(files[2].split()[int(ind[2])-1]))
 enemy_pic = tkinter.PhotoImage(file="images/"+str(files[1].split()[int(ind[1])-1]))
-label = tkinter.Label(master, text="Найди выход")
 label.pack()
 canvas = tkinter.Canvas(master, bg='black', height=N_X * step, width=N_Y * step)
 canvas.pack()
